169964c74ef6a9bb30ee7a4b1db8bd26a89ab5e7
rubencodex86 <133975993+rubencodex86@users.noreply.github.com>
2024-10-16 14:40:15 +0000

    Pi exercise - vpl edition

--- 1ano/IP/01_actividade_1/exercicios/pi.c
+++ 1ano/IP/01_actividade_1/exercicios/pi.c
@@ -1,3 +1,75 @@
+/*VPL*/
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int k;
+    double pi;
+
+    // get k
+    printf(" ");
+    scanf("%d", & k);
+
+    // 2sqrt(2)/9801
+    double raiz = ((2 * sqrt(2)) / 9801);
+
+    double somatorio = 0;
+
+    // loop somatorio
+    int current_k = 0;
+    while (current_k <= k) {
+        // 4k! * (1103 + 26390 * k)
+        int i = 1;
+        double fatorial = 1;
+        double quatrok = 1;
+
+        // calcula 4k!
+        while (i <= 4 * current_k) {
+            fatorial *= i;
+            i++;
+        }
+        quatrok = fatorial;
+
+        // big multiplication
+        double bigmulti = (1103 + (26390 * current_k));
+        double numerador = quatrok * bigmulti;
+
+        // denominador: (k!)^4 * (396)^(4k)
+        i = 1;
+        fatorial = 1;
+
+        // calcula k!
+        while (i <= current_k) {
+            fatorial *= i;
+            i++;
+        }
+        double kfact = pow(fatorial, 4);
+
+        // calcula (396)^(4k)
+        double potencia = pow(396, 4 * current_k);
+
+        // denominador
+        double denominador = kfact * potencia;
+
+        // division por current_k
+        double divisao = numerador / denominador;
+
+        somatorio += divisao;
+
+        current_k++;
+    }
+
+    // calcula pi
+    pi = 1 / (raiz * somatorio);
+    printf("%.17g ",pi);
+
+    return 0;
+}
+
+
+
+/* last version
 #include <stdio.h>
 #include <math.h>
 
@@ -67,7 +139,7 @@ int main()
 
     return 0;
 }
-
+*/
 
 
 /* 1st attempt
