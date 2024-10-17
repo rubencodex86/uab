e3bd448fde05f7d13f8faa6a478f039558407635
rubencodex86 <133975993+rubencodex86@users.noreply.github.com>
2024-10-17 12:36:51 +0000

    Formula resolvente - first commit

---
+++ 1ano/IP/01_actividade_1/exercicios/formularesolvente.c
@@ -0,0 +1,15 @@
+/*
+Faça um programa que peça os coeficientes de um polinómio do segundo grau, e retorna as
+raízes reais, caso existam. Adicionalmente o programa deve retornar todos os conjuntos de
+coeficientes inteiros, que têm apenas raízes inteiras reais. Os coeficientes estão entre -K e K não
+tendo nenhum coeficiente nulo (K é introduzido pelo utilizador e é um inteiro pequeno). Relembra-
+se a fórmula resolvente:
+
+ax²bx+c=0 <=> x=(-b +- sqrt(b²-4ac))/2a
+
+Notas:
+- Para calcular a raiz quadrada, utilize a função sqrt, disponível na biblioteca math.h
+- Faça três ciclos um dentro do outro, um ciclo por cada um dos coeficientes, e varie a variável
+  iteradora entre -K e K.
+*/
+
